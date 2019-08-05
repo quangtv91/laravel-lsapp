87bc327f859f2487ea265183a160cfd617bafebb
unknown <quangcoltech@gmail.com>
2019-08-06 02:03:07 +0700

    create app, first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+  <h1>{{$title}}</h1>
+  <p>This is Laravel app</p>
+  <p>
+    <button type="button" class="btn btn-primary" href="/login">Login</button>
+    <button type="button" class="btn btn-success" href="/register">Register</button>
+  </p>
+</div>
+@stop
+
